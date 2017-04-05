3402a1bd71cd55e47ae7b09271169ef6630cbeec
BoLiangZifei <sky2007hzm@126.com>
2017-04-05 16:18:29 +0800

    first commit

--- TestGit/TestGit/ViewController.m
+++ TestGit/TestGit/ViewController.m
@@ -17,6 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString * strokedImagepath=@"/Users/apl/Desktop/image/6D53778915C23627891AC0EDE85F640E.jpg";
+    
+    UIImage *imgFromUrl=[[UIImage alloc]initWithContentsOfFile:strokedImagepath];
+    NSData *dataObj = UIImageJPEGRepresentation(imgFromUrl, 0.1);
+    NSLog(@"字符串--%@",dataObj);
+    Byte *testByte = (Byte *)[dataObj bytes];
+//    for(int i=0;i<[dataObj length];i++)
+//    {
+////        printf("testByte = %d\n",testByte[i]);
+//    }
+//   
+    NSString *encodedImageStr = [dataObj base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    NSLog(@"----%@",encodedImageStr);
+    NSData *_decodedImageData   = [[NSData alloc] initWithBase64Encoding:encodedImageStr];
+    UIImage *_decodedImage      = [UIImage imageWithData:_decodedImageData];
+    
+//        UIImage *image = [UIImage imageWithData: dataObj];
+
+//    NSString *aString = [[NSString alloc] initWithData:dataObj encoding:NSUTF8StringEncoding];
+
+//    NSData *adata = [[NSData alloc] initWithBytes:testByte length:10241024];
+    UIImage *image = [UIImage imageWithData: dataObj];
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0,0 , 800, 800)];
+    [imageView setImage:_decodedImage];
+    [self.view addSubview:imageView];
+
 }
 
 - (void)didReceiveMemoryWarning {
